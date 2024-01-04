fbfaa3a27878e5b4dcfa2b2e699d277ce4646326
barbod garshas <barbodgarshasbi79@gmail.com>
2024-01-04 17:47:17 +0100

    Project Setup

---
+++ wordle/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<template>
+  <TheHeader>
+    
+  </TheHeader>
+  <main>
+  </main>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ wordle/src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const headerClassList = ref("header")
+
+</script>
+
+<template>
+    <header :class="headerClassList">
+        
+    </header>
+</template>
+
+<style lang="scss">
+
+.header {
+}
+
+</style>
