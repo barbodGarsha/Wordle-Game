76bbdca0b60e0df284b446a0c80f28afab1792c4
barbod garshas <barbodgarshasbi79@gmail.com>
2024-01-08 14:57:14 +0100

    code clean up

--- wordle/src/App.vue
+++ wordle/src/App.vue
@@ -3,13 +3,45 @@ import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
-  <TheHeader>
-    
-  </TheHeader>
+  <TheHeader></TheHeader>
+  
   <main>
   </main>
 </template>
 
 <style lang="scss">
+  
+  //RESETS
+  html {
+      box-sizing: border-box;
+  }
+    
+  *, *:before, *:after {
+      box-sizing: inherit;
+  }
 
+  body {
+      background-color: white;
+      margin: 0;
+      padding: 0;
+      font-family: sans-serif;
+  }
+  a, li, h1, h2, p {
+      text-decoration: none;
+      margin: 0;
+      padding: 0;
+      z-index: 9999;
+  }
+
+  ul {
+      display: inline;  
+      margin: 0;
+      padding: 0;
+  }
+  li {
+      display: inline;
+      list-style-type: none;
+      margin: 0;
+      padding: 0; 
+  }
 </style>
