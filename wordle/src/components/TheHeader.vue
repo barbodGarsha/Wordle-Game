0e397eb3626791cc33e8f737bc8f56232fe6980a
barbod garshas <barbodgarshasbi79@gmail.com>
2024-01-08 14:57:24 +0100

    TODO added

--- wordle/src/components/TheHeader.vue
+++ wordle/src/components/TheHeader.vue
@@ -2,19 +2,34 @@
 
 import { ref } from 'vue'
 
+//TODO: the const classes like the ones below should be hard coded into the template but the modifiers better be using refs instead
 const headerClassList = ref("header")
+const header__titleClassList = ref("header__title")
 
 </script>
 
 <template>
     <header :class="headerClassList">
-        
+        <p :class="header__titleClassList">Wordle</p>
     </header>
 </template>
 
 <style lang="scss">
 
 .header {
+    background-color: beige;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100vw;
+    
+    padding: 2rem;
+
+    .header__title {
+        font-size: 2rem;
+    }
 }
 
 </style>
