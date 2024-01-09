43c2c8e5d4b82711ba680a012d89f5f6fe73bad0
barbod garshas <barbodgarshasbi79@gmail.com>
2024-01-09 16:40:39 +0100

    base of the Row component is ready

---
+++ wordle/src/components/Row.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="row">
+    <slot>
+        
+    </slot>
+</div>
+</template>
+
+<style lang="scss">
+
+    .row {
+        display: flex;
+    }
+
+</style>
