c90748812398a00fe31085fda92dc8a68ac310e1
barbod garshas <barbodgarshasbi79@gmail.com>
2024-01-09 16:59:54 +0100

    Key and Keyboard components bases are finished

---
+++ wordle/src/components/Key.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="key">
+    <p class="key__p">
+        <slot></slot>
+    </p>
+</div>
+</template>
+
+<style lang="scss">
+
+.key {
+    cursor: pointer;
+    background-color: chartreuse;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 5rem;
+    height: 5rem;
+
+    .key__p {
+        font-size: 2rem;
+    }   
+}
+
+</style>
---
+++ wordle/src/components/Keyboard.vue
@@ -0,0 +1,53 @@
+
+<script setup>
+
+import Key from './Key.vue'
+
+</script>
+
+<template>
+    <div class="keyboard">
+        <div class="keyboard__row">
+            <Key>Q</Key>
+            <Key>W</Key>
+            <Key>E</Key>
+            <Key>R</Key>
+            <Key>T</Key>
+            <Key>Y</Key>
+            <Key>U</Key>
+            <Key>I</Key>
+            <Key>O</Key>
+            <Key>P</Key>
+        </div>
+        <div class="keyboard__row">
+            <Key>A</Key>
+            <Key>S</Key>
+            <Key>D</Key>
+            <Key>F</Key>
+            <Key>G</Key>
+            <Key>H</Key>
+            <Key>J</Key>
+            <Key>K</Key>
+            <Key>L</Key>
+        </div>
+        <div class="keyboard__row">
+            <Key>Z</Key>
+            <Key>X</Key>
+            <Key>C</Key>
+            <Key>V</Key>
+            <Key>B</Key>
+            <Key>N</Key>
+            <Key>M</Key>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+
+    .keyboard {
+        .keyboard__row {
+            display: flex;
+        }   
+    }
+
+</style>
