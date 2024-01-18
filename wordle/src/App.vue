4b16485eb7eb5c2e44d4a1c32cb51e20c6e4816e
barbod garshas <barbodgarshasbi79@gmail.com>
2024-01-18 17:05:43 +0100

    new font

--- wordle/src/App.vue
+++ wordle/src/App.vue
@@ -157,6 +157,8 @@ document.addEventListener('keydown', (e) => {
 
 <style lang="scss">
   
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap');
+
   //RESETS
   html {
       box-sizing: border-box;
@@ -170,7 +172,7 @@ document.addEventListener('keydown', (e) => {
       background-color: white;
       margin: 0;
       padding: 0;
-      font-family: sans-serif;
+      font-family: 'Poppins', sans-serif;
   }
   a, li, h1, h2, p {
       text-decoration: none;
