a681836403a811cdf3a915f18c4f60b27242a729
barbod garshas <barbodgarshasbi79@gmail.com>
2024-02-04 12:28:41 +0100

    cookies are now ready to work with

--- wordle/src/App.vue
+++ wordle/src/App.vue
@@ -179,6 +179,35 @@ document.addEventListener('keydown', (e) => {
   }
 })
 
+//COOKIES ===============================================================================
+function setCookie(name, value, ex) {
+  const date = new Date();
+  date.setTime(date.getTime() + (ex*24*60*60*1000));
+  let expires = "expires="+ date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(cookieName) {
+  let name = cookieName + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function deleteCookie(cookieName) {
+  document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+
 // MAIN =================================================================================
 newGameInit()
 
@@ -319,7 +348,7 @@ newGameInit()
       font-weight: bold;
     }
   }
-  
+
   .main {
     display: flex;
     flex-direction: column;
